6a70d4fab6b0c872993952247807bc6d66a2445a
danielmurilo <danielmurilo@yahoo.com.br>
2023-03-08 16:46:24 -0300

    DesafioFabricadeCarros

---
+++ app/src/main/java/me/dio/DesafiosDIO/DesafioFabricadeCarros.java
@@ -0,0 +1,22 @@
+package me.dio.DesafiosDIO;
+
+import java.util.Scanner;
+
+public class DesafioFabricadeCarros {
+
+    public static void main(String[] args) {
+
+	    Scanner scan = new Scanner(System.in);
+        int custoFabrica = scan.nextInt();
+	    int porcentagemDistribuidor = scan.nextInt();
+	    int percentualImpostos = scan.nextInt();
+        scan.close();
+      
+        int distribuidor = (custoFabrica * porcentagemDistribuidor) / 100;
+        int valorImpostos = (custoFabrica * percentualImpostos) / 100;        
+        int custoConsumidor = custoFabrica + distribuidor + valorImpostos;
+    
+        System.out.println(custoConsumidor);
+	}
+    
+}
